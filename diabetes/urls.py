ef0d0d4410a77415d8b82d28c862f74da7d3d76e
grael@comcast.net <cjgtsm79!>
2015-11-19 10:54:48 -0700

    added url for the registration templates

--- diabetes/urls.py
+++ diabetes/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     # Examples:
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', 'diabetes_manager.views.index', name='index'),
+    url(r'^account/', include('registration.backends.default.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
